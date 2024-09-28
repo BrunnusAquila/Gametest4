f13b3c4721a4d6203492c7dde0e4aaaac7b0947c
B. G. B. B. <brunnobarbosabarros@gmail.com>
2024-09-28 02:01:16 -0300

    Delete GAME directory

--- GAME/Scripts/level_1.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	$AudioStreamPlayer2D.play()
-	$AudioStreamPlayer2D.volume_db = -0.0
--- GAME/Scripts/pause.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas/Level_1.tscn")
-	pass # Replace with function body.
-
-func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file(("res://Cenas/Main.tscn"))
-	pass # Replace with function body.
