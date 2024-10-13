a6605455063f60f039e8caa04a9667ba73a852a7
B. G. B. B. <brunnobarbosabarros@gmail.com>
2024-10-12 23:25:31 -0300

    Delete GAME directory

--- GAME/Scripts/bricks_blue.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready():
-	for child in get_children():
-		child.add_to_group("Brickzes")
-		child.add_to_group("Bricks")
--- GAME/Scripts/item.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var grupo_pau = "pauzinho"
-
-func _ready():
-	for child in get_children():
-		child.add_to_group("WOW")
